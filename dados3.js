ef23ee9b9eba49a6675d14b83f96bde45190dea2
karinabr1 <86989226+karinabr1@users.noreply.github.com>
2022-01-24 21:34:28 -0300

    Add files via upload

---
+++ dados3.js
@@ -0,0 +1,11 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario)
+
+let estachovendo = true // or false
+console.log(typeof estachovendo);
+
+console.log(typeof "Teste");
