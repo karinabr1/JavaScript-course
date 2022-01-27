2a5522d99f244ab49e170d71375dbf0e9d8a1b01
karinabr1 <86989226+karinabr1@users.noreply.github.com>
2022-01-26 21:24:20 -0300

    Add files via upload

---
+++ tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nomes genericos e siglas
+ /* Exemplos
+ let valor = ''
+ let numero = ''
+ let pqp = false "Produto Quimico Perigoso... kkk" */
